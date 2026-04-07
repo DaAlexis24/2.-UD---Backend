f04085a4af3a11efd3192655b88b054941283d65
Daniel Soledad <danielalsole24@gmail.com>
2026-04-07 14:00:40 +0200

    Add types for other views

---
+++ 7.Challenge/backend/src/views/types/view.ts
@@ -0,0 +1,13 @@
+import { readFileSync } from 'node:fs';
+
+export abstract class View {
+  template!: string;
+  css!: string;
+
+  constructor(name: string) {
+    this.css = readFileSync(`./src/views/${name}/${name}.css`, {
+      encoding: 'utf-8',
+    });
+  }
+  abstract setTemplate(): void;
+}
