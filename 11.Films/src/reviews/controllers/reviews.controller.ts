9dbe9ef2f94a19aa926b8a0b7ecbd6a92a630153
Daniel Soledad <danielalsole24@gmail.com>
2026-05-08 09:53:13 +0200

    Update reviews Feature

--- 11.Films/src/reviews/controllers/reviews.controller.ts
+++ 11.Films/src/reviews/controllers/reviews.controller.ts
@@ -2,9 +2,15 @@ import { env } from '../../config/env.ts';
 import debug from 'debug';
 import type { ReviewsRepo } from '../repo/reviews.repo.ts';
 import type { Request, Response, NextFunction } from 'express';
+import { InternalServerError, NotFoundError } from '../../errors/http-error.ts';
+import { PrismaClientKnownRequestError } from '@prisma/client/runtime/client';
+import type {
+  ReviewCreateBodyDTO,
+  ReviewUpdateDTO,
+} from '../entities/review.dto.ts';
 
 const log = debug(`${env.PROJECT_NAME}:controller:reviews`);
-log('Loading Reviews Controller');
+log('Loading reviews controller...');
 
 export class ReviewsController {
   #repo: ReviewsRepo;
@@ -14,12 +20,28 @@ export class ReviewsController {
 
   async getAllFilmsReviews(req: Request, res: Response, next: NextFunction) {
     try {
-      const reviews = await this.#repo.getAllFilmReviews(
+      const reviews = await this.#repo.getAllFilmsReviews(
         Number(req.params.filmID),
       );
       return res.json(reviews);
     } catch (error) {
-      next(error);
+      if (
+        error instanceof PrismaClientKnownRequestError &&
+        error.code === 'P2025'
+      ) {
+        const notFoundError = new NotFoundError('Film requested not found', {
+          cause: error,
+        });
+        log('Error getting all film reviews: %s', notFoundError.message);
+        return next(notFoundError);
+      }
+
+      const internalError = new InternalServerError(
+        'Failed to get all film reviews',
+        { cause: error },
+      );
+      log('Error getting all film reviews: %s', internalError.message);
+      next(internalError);
     }
   }
 
@@ -30,20 +52,99 @@ export class ReviewsController {
       );
       return res.json(reviews);
     } catch (error) {
-      next(error);
+      if (
+        error instanceof PrismaClientKnownRequestError &&
+        error.code === 'P2025'
+      ) {
+        const notFoundError = new NotFoundError('User requested not found', {
+          cause: error,
+        });
+        log('Error getting all user reviews: %s', notFoundError.message);
+        return next(notFoundError);
+      }
+
+      const internalError = new InternalServerError(
+        'Failed to get all user reviews',
+        { cause: error },
+      );
+      log('Error getting all user reviews: %s', internalError.message);
+      next(internalError);
     }
   }
 
+  // - POST /reviews/:filmId/ [User] -> token :userId
+
   async createReview(req: Request, res: Response, next: NextFunction) {
     try {
+      const reviewData: ReviewCreateBodyDTO = req.body;
       const review = await this.#repo.createReview({
-        ...req.body,
-        filmID: Number(req.params.filmID), // ruta
-        userID: Number(req.user?.id), // token
+        ...reviewData,
+        filmID: Number(req.params.filmID),
+        userID: Number(req.user?.id),
       });
       return res.status(201).json(review);
     } catch (error) {
-      next(error);
+      const internalError = new InternalServerError('Failed to create review', {
+        cause: error,
+      });
+      log('Error creating review: %s', internalError.message);
+      next(internalError);
+    }
+  }
+
+  async updateReview(req: Request, res: Response, next: NextFunction) {
+    try {
+      const reviewData: ReviewUpdateDTO = req.body;
+      const review = await this.#repo.updateReview(
+        Number(req.user?.id),
+        Number(req.params.filmID),
+        reviewData,
+      );
+      return res.json(review);
+    } catch (error) {
+      if (
+        error instanceof PrismaClientKnownRequestError &&
+        error.code === 'P2025'
+      ) {
+        const notFoundError = new NotFoundError('Review requested not found', {
+          cause: error,
+        });
+        log('Error updating review: %s', notFoundError.message);
+        return next(notFoundError);
+      }
+
+      const internalError = new InternalServerError('Failed to update review', {
+        cause: error,
+      });
+      log('Error updating review: %s', internalError.message);
+      return next(internalError);
+    }
+  }
+
+  async deleteReview(req: Request, res: Response, next: NextFunction) {
+    try {
+      await this.#repo.deleteReview(
+        Number(req.user?.id),
+        Number(req.params.filmID),
+      );
+      return res.status(204).send();
+    } catch (error) {
+      if (
+        error instanceof PrismaClientKnownRequestError &&
+        error.code === 'P2025'
+      ) {
+        const notFoundError = new NotFoundError('Review requested not found', {
+          cause: error,
+        });
+        log('Error deleting review: %s', notFoundError.message);
+        return next(notFoundError);
+      }
+
+      const internalError = new InternalServerError('Failed to delete review', {
+        cause: error,
+      });
+      log('Error deleting review: %s', internalError.message);
+      return next(internalError);
     }
   }
 }
--- 11.Films/src/reviews/repo/reviews.repo.ts
+++ 11.Films/src/reviews/repo/reviews.repo.ts
@@ -3,12 +3,16 @@ import { env } from '../../config/env.ts';
 import debug from 'debug';
 import type {
   ReviewCreateDTO,
-  ReviewParamsDTO,
   ReviewUpdateDTO,
-} from '../../zod/film.schema.ts';
+} from '../entities/review.dto.ts';
+import type {
+  FilmReview,
+  ReviewBase,
+  UserReview,
+} from '../entities/review.entity.ts';
 
 const log = debug(`${env.PROJECT_NAME}:repo:reviews`);
-log('Loading Reviews Repo');
+log('Loading reviews repo...');
 
 export class ReviewsRepo {
   #prisma: AppPrismaClient;
@@ -16,9 +20,8 @@ export class ReviewsRepo {
     this.#prisma = prisma;
   }
 
-  // Obtenemos todos los reviews de una película
-  async getAllFilmReviews(filmID: number) {
-    log('Getting all reviews by film with id: %d', filmID);
+  async getAllFilmsReviews(filmID: number): Promise<FilmReview[]> {
+    log('Getting all reviews of film %s', filmID);
     return await this.#prisma.review.findMany({
       where: {
         filmID,
@@ -47,18 +50,63 @@ export class ReviewsRepo {
     });
   }
 
-  // Obtenemos todos los reviews de un Usuario
-  async getAllUserReviews(userID: number) {
-    log('Getting all reviews by user with id: %d', userID);
+  async getAllUserReviews(userID: number): Promise<UserReview[]> {
+    log('Getting all reviews of user with id %s', userID);
     return await this.#prisma.review.findMany({
       where: {
         userID,
       },
       omit: {
-        userID: true,
         filmID: true,
+        userID: true,
       },
       include: {
+        user: {
+          select: {
+            profile: {
+              select: {
+                firstName: true,
+                surname: true,
+              },
+            },
+          },
+        },
+        film: {
+          select: {
+            title: true,
+            year: true,
+            director: true,
+          },
+        },
+      },
+    });
+  }
+
+  async createReview(data: ReviewCreateDTO): Promise<FilmReview> {
+    log('Creating review for film %s by user %s', data.filmID, data.userID);
+    return await this.#prisma.review.create({
+      data: {
+        review: data.review,
+        rate: data.rate,
+        // date: new Date(),
+        filmID: data.filmID,
+        userID: data.userID,
+      },
+      omit: {
+        filmID: true,
+        userID: true,
+      },
+      include: {
+        user: {
+          select: {
+            profile: {
+              select: {
+                firstName: true,
+                surname: true,
+              },
+            },
+          },
+        },
         film: {
           select: {
             title: true,
@@ -68,23 +116,57 @@ export class ReviewsRepo {
     });
   }
 
-  // Creamos un Review
-  async createReview(data: ReviewCreateDTO) {
-    log('Creating review for film %s by user %s', data.filmID, data.userID);
-    return await this.#prisma.review.create({
-      data: {
-        review: data.review,
-        rate: data.rate,
-        date: new Date(),
-        filmID: data.filmID,
-        userID: data.userID,
+  async updateReview(
+    userID: number,
+    filmID: number,
+    data: ReviewUpdateDTO,
+  ): Promise<FilmReview> {
+    log('Updating review for film %s by user %s', filmID, userID);
+    return await this.#prisma.review.update({
+      where: {
+        userID_filmID: {
+          userID,
+          filmID,
+        },
+      },
+      data,
+      omit: {
+        filmID: true,
+        userID: true,
+      },
+      include: {
+        user: {
+          select: {
+            profile: {
+              select: {
+                firstName: true,
+                surname: true,
+              },
+            },
+          },
+        },
+        film: {
+          select: {
+            title: true,
+          },
+        },
       },
     });
   }
 
-  // [PATCH] /api/reviews/:filmId/ (Owner) => Token: userId
-  async updateReview(id: ReviewParamsDTO, data: ReviewUpdateDTO) {
-    log('Updating review for film %s by user %d', id.filmID, id.userID);
-    return await this.#prisma.review.update({});
+  async deleteReview(userID: number, filmID: number): Promise<ReviewBase> {
+    log('Deleting review for film %s by user %s', filmID, userID);
+    return await this.#prisma.review.delete({
+      where: {
+        userID_filmID: {
+          userID,
+          filmID,
+        },
+      },
+      omit: {
+        filmID: true,
+        userID: true,
+      },
+    });
   }
 }
